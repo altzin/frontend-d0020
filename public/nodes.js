24943f4761b00de64fc01adfd357e8a4fd44ab70
Oscar Lundberg <oscarlundberg95@hotmail.se>
2020-03-06 15:01:18 +0100

    nodes working

---
+++ public/nodes.js
@@ -0,0 +1,195 @@
+var dummy = 0;
+var width = window.innerWidth,
+height = 600;
+height = 350;
+
+var colorScale = ['orange', 'lightblue', '#B19CD9'];
+var xScale = d3.scaleLinear().domain([0, 1]).range([0, 1000]);
+
+var svg = d3.select('target').append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+
+var links = [
+    {source: 0, target: 1},
+    {source: 1, target: 2},
+    {source: 2, target: 3},
+    {source: 3, target: 4},
+    //{source: 4, target: 5}
+];
+
+var numNodes = 5;
+var nodes = d3.range(numNodes).map(function (d, i) {
+    return {
+        radius: 30,
+        value: 0.2 * i,
+        index: i,
+        clicked: 0
+    }
+});
+
+var simulation = d3.forceSimulation(nodes)
+
+    .force('collision', d3.forceCollide().radius(function (d) {
+        return d.radius;
+    }))
+    .force("center", d3.forceCenter().x(width / 2).y(height / 2))
+    .force('link', d3.forceLink().links(links))
+    .force('x', d3.forceX(function(d) {
+        return xScale(d.value);
+    }).strength(2))
+    .force('y', d3.forceY(0).strength(0))
+    .on('tick', ticked);
+
+/*
+//Initialize the pie when entering the page
+function initPie() {
+    console.log("vafan")
+    d3.csv("nodes/node1.csv").then(function (data) {
+        updatePie({a: data[currentEvent].MAP, b: 1-data[currentEvent].MAP})
+
+    })
+    console.log("benis")
+    eventNumberToHtml()
+}
+
+ */
+
+
+function updateLinks() {
+    var u = d3.select('svg')
+        .selectAll('line')
+        .data(links);
+    u.enter()
+        .append('line')
+        .merge(u)
+        .attr('x1', function(d) {
+            return d.source.x
+        })
+        .attr('y1', function(d) {
+            return d.source.y
+        })
+        .attr('x2', function(d) {
+            return d.target.x
+        })
+        .attr('y2', function(d) {
+            return d.target.y
+        });
+    u.exit().remove()
+}
+function updateNodes() {
+    var u = d3.select('svg')
+        .selectAll('circle')
+        .data(nodes);
+
+    u.enter()
+        .append('circle')
+        .attr('r', function (d) {
+            return d.radius;
+        })
+        .style('fill', function (d) {
+            return d.color;
+        })
+        .merge(u)
+        .attr('cx', function (d) {
+            return d.x;
+        })
+        .attr('cy', function (d) {
+            return d.y;
+        })
+    u.on("click", function(d,i) {
+        // if(dummy == 0){
+        //     initPie();
+        //     dummy++;
+        // }
+        // else
+        if(d.clicked == 0)
+        {
+            d3.select(this).style("fill","lightcoral");
+            d3.select(this).attr('r', d.radius*2);
+            nodeMarked = i;
+            csvFile();
+            showPie();
+            d.clicked++;
+        }
+        else
+        {
+            d3.select(this).style("fill","#000");
+            d3.select(this).attr('r', d.radius);
+            removePie();
+            d.clicked--;
+        }
+    });
+
+    u.exit().remove();
+}
+
+nodeMarked = -1;
+
+function csvFile() {
+    d3.csv("nodes/node"+nodeMarked+".csv").then(function (data) {
+        updatePie({a: data[currentEvent].MAP, b: 1-data[currentEvent].MAP});
+        updateBar([
+            {group: `Event ${currentEvent}`, value: data[currentEvent].MAP},
+            {group: `Event ${currentEvent+1}`, value: data[currentEvent+1].MAP},
+            {group: `Event ${currentEvent+2}`, value: data[currentEvent+2].MAP}]);
+        print(data);
+        console.log("fan");
+    })
+}
+let currentEvent=0;
+
+function nextEvent() {
+    currentEvent++;
+    csvFile();
+    eventNumberToHtml()
+}
+
+function previousEvent() {
+    if (currentEvent > 1) {
+        currentEvent--;
+    } else {
+        currentEvent = 0;
+    }
+    csvFile();
+    eventNumberToHtml()
+
+}
+function resetEvent(){
+    currentEvent = 0;
+    d3.csv("node.csv").then(function (data) {
+        updatePie({a: data[currentEvent].MAP, b: 1-data[currentEvent].MAP})
+    })
+    eventNumberToHtml()
+}
+//Event input field
+// when the input range changes update value
+d3.select("#nValue").on("input", function() {
+    updateInput(+this.value);
+});
+
+// Initial update value
+updateInput(0);
+
+// adjust the text
+function updateInput(nValue) {
+    currentEvent = nValue;
+    d3.csv("node.csv").then(function (data) {
+        updatePie({a: data[currentEvent].MAP, b: 1-data[currentEvent].MAP})
+    })
+    eventNumberToHtml()
+}
+
+function print(data) {
+    console.log(data);
+}
+
+function ticked() {
+    updateLinks();
+    updateNodes();
+}
+
+function eventNumberToHtml() {
+    document.getElementById("output").innerHTML = currentEvent;
+}
--- public/piechart.js
+++ public/piechart.js
@@ -1,36 +1,35 @@
 // set the dimensions and margins of the graph
-var width = 300
-height = 300
-margin = 10
+var width = 450
+height = 450
+margin = 40
 
 // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
 var radius = Math.min(width, height) / 2 - margin
 
-// append the svg object to the div called 'my_dataviz'
+// append the svg object to the div called 'my_dataviz2'
 var svg = d3.select('#target2').append('svg')
+    .append("tja")
     .attr("width", width)
     .attr("height", height)
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-var data = { a: 9, b: 20, c: 30, d: 8, e: 12 }
-
 // set the color scale
 var color = d3.scaleOrdinal()
-    .domain(data)
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56"])
-
+    .domain(["a", "b", "c", "d", "e", "f"])
+    .range(d3.schemeDark2);
 
 // A function that create / update the plot for a given variable:
 function updatePie(data) {
+    console.log(data);
     // Compute the position of each group on the pie:
     var pie = d3.pie()
-        .value(function (d) { return d.value; })
-        .sort(function (a, b) { console.log(a); return d3.ascending(a.key, b.key); }) // This make sure that group order remains the same in the pie chart
+        .value(function(d) {return d.value; })
+        .sort(function(a, b) { console.log(a) ; return d3.ascending(a.key, b.key);} ) // This make sure that group order remains the same in the pie chart
     var data_ready = pie(d3.entries(data))
 
     // map to data
-    var u = svg.selectAll("path")
+    var u = svg1.selectAll("path")
         .data(data_ready)
 
     // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
@@ -39,13 +38,13 @@ function updatePie(data) {
         .append('path')
         .merge(u)
         .transition()
-        .duration(1000)
+        .duration(0)
         .attr('d', d3.arc()
             .innerRadius(0)
             .outerRadius(radius)
         )
-        .attr('fill', function (d) { return (color(d.data.key)) })
-        .attr("stroke", "black")
+        .attr('fill', function(d){ return(color(d.data.key)) })
+        .attr("stroke", "white")
         .style("stroke-width", "2px")
         .style("opacity", 1)
 
@@ -53,7 +52,13 @@ function updatePie(data) {
     u
         .exit()
         .remove()
-}
-updatePie(data)
 
-// Initialize the plot with the first dataset
+}
+function removePie() {
+    d3.select("tja").style("opacity", 0);
+    d3.select("tja2").style("opacity", 0);
+}
+function showPie() {
+    d3.select("tja").style("opacity", 1);
+    d3.select("tja2").style("opacity", 1);
+}
